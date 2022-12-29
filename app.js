892f5a8c85e3a35c324e2e53ceac7cc6c4cdb04e
pradumn <pradumnprasad883@gmail.com>
2022-12-29 22:57:50 +0530

    Fixed bug for connection of Database.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require("lodash");
 const dotenv=require("dotenv");
-
+dotenv.config({path:'config.env'});
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -15,7 +15,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-dotenv.config({path:"./config.env"});
+
 
 const DB=process.env.DATABASE;
 
